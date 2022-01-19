efe5af55b85a18a3a12d4a1584b0252c8260f614
BushsHaxs <72326501+BushsHaxs@users.noreply.github.com>
2022-01-19 17:50:25 -0600

    i hope this fixed it

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -452,20 +452,17 @@ class StoryMenuState extends MusicBeatState
 					intendedScore = Highscore.getWeekScore(curWeek, curDifficulty);
 					#end
 				}
-
-				override function beatHit
-				}
-					super.beatHit();
+			}
+				super.beatHit();
 
 				if (curBeat % 2 == 0)
 				{
 					grpWeekCharacters.members[0].bopHead();
 					grpWeekCharacters.members[1].bopHead();
 				}
-				else if (weekCharacters[curWeek][0] == 'spooky' || weekCharacters[curWeek][0] == 'gf')
-					grpWeekCharacters.members[0].bopHead();
+				else if (weekCharacters[curWeek][0] == 'spooky'
+					|| weekCharacters[curWeek][0] == 'gf') grpWeekCharacters.members[0].bopHead();
 
-				if (weekCharacters[curWeek][2] == 'spooky' || weekCharacters[curWeek][2] == 'gf')
-					grpWeekCharacters.members[2].bopHead();
-			}
+				if (weekCharacters[curWeek][2] == 'spooky' || weekCharacters[curWeek][2] == 'gf') grpWeekCharacters.members[2].bopHead();
 		}
+	}

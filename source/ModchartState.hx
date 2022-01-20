0bd5ebefa1a079ffda85ce39c6acb007e448dd56
BushsHaxs <72326501+BushsHaxs@users.noreply.github.com>
2022-01-20 17:29:27 -0600

    removed #if FEATURE_LUAMODCHART

--- source/ModchartState.hx
+++ source/ModchartState.hx
@@ -1,6 +1,5 @@
 // this file is for modchart things, this is to declutter playstate.hx
 // Lua
-#if FEATURE_LUAMODCHART
 import LuaClass.LuaGame;
 import LuaClass.LuaWindow;
 import LuaClass.LuaSprite;
